6df7ddf299fa152ee0e3335f360169ded9a90551
jianglin521 <17610835880@163.com>
2019-04-03 14:53:20 +0800

    修改

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,16 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jianglin521.git master:gh-pages
+# 如果发布到 https://<USERNAME>.github.io
+ git push -f git@github.com:jianglin521/jianglin521.github.io.git master
 
-cd -
